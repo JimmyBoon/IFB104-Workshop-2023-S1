fc7d3d08fef3b20dd429893fa6f790f512d25ad7
James Boon <jwboon@pm.me>
2023-05-16 14:55:01 +1000

    End of workshop 11

---
+++ Workshop_11/Task1/backend_function.py
@@ -0,0 +1,133 @@
+#--------------------------------------------------------------------#
+#
+# Staff Directory Back-End
+#
+# This is where you will develop the back-end function for returning
+# employee details that match the given first/last-name prefix. Given
+# a character string you must return details of all employees whose
+# first-name, last-name or both match the string.  You will need to
+# confer with the front-end team to agree on the format in which the
+# results are returned, but for each matching employee they must
+# contain at least the (1) employee number, (2) first name, (3) last
+# name and (4) date of birth.
+#
+# The employee data can be extracted from either the SQLite database
+# or the CSV file provided.  Both contain the same employee data.
+# Three solutions are possible:
+#
+# 1) Write a Python function which iterates over the lines of text
+# in the CSV file to find matching employees using character string
+# operations.  In this case recall that it's easy to iterate over
+# each line in a text file using a "for" loop in Python.  Your
+# solution will need to use the built-in file "open" function, the
+# character string "split" method to extract the individual fields
+# from each line (splitting the text on commas), and the string
+# "startswith" predicate to determine if a first-name or last-name
+# begins with the prefix entered by the user.
+#
+# 2) Write a Python function which uses the regular expression
+# "findall" function to find and return all employee records with
+# a first-name or last-name that begins with the prefix entered
+# by the user.  In this case you will need the built-in "open" function
+# and "read" method to get the contents of the CSV file as a single
+# character string, and the "findall" method to find matching employee
+# data.  You will find this quite easy if you use findall's MULTILINE
+# option (as a third argument in the call) which allows you to
+# use ^ and $ to match the beginning and end of each line of text
+# in the string, respectively.
+#
+# 3) Write a Python function which accesses the SQLite database copy
+# of the employee data.  In this case you will need to use the
+# usual SQLite methods for opening and closing the database,
+# executing a query on its contents, and fetching the result set.
+# The query in this case can use the "C LIKE 'P%'" Boolean
+# expression to match rows in which a text-valued column C begins
+# with text pattern P.
+#
+
+# Import the findall function and the MULTILINE constant
+# so that we can use ^ and $ in our regular expression to
+# match the beginning and end of each line of text, if
+# desired
+from re import findall, MULTILINE, IGNORECASE
+
+# Import the SQLite database functions
+from sqlite3 import *
+from os.path import exists
+
+# Define your back-end function here
+def search_by_loop(search):
+    
+    with open('employees.csv') as file:
+        lines = file.readlines()
+
+    lines.pop(0)
+
+    results = []
+
+    for line in lines:
+        emp_no, birth_date, first_name, last_name, gender, hire_date = line.split(',')
+        if first_name.lower().startswith(search.lower()) or last_name.lower().startswith(search.lower()):
+            results.append((emp_no, first_name, last_name, birth_date))
+
+    return results
+
+def search_by_regex(search):
+
+    with open('employees.csv') as file:
+        content = file.read()
+
+    regex = f"(.+\\b{search}.+)"
+
+    content = findall(regex, content, IGNORECASE)
+
+    return content
+
+#Make a connection to the db
+def search_by_db(search):
+    db_name = "employees.db"
+    connection = connect(database=db_name)
+    db = connection.cursor()
+
+    db.execute(f'SELECT ALL * FROM employees WHERE first_name LIKE "{search}%" or last_name LIKE "{search}%"')
+
+    result = db.fetchall()
+
+    db.close()
+    connection.close()
+
+    return result
+
+def create_database():
+    db_name = "employees.db"
+    connection = connect(database=db_name)
+    db = connection.cursor()
+
+    db.execute("CREATE TABLE Ratings(RatingID INTEGER UNIQUE primary key AUTOINCREMENT, EmpId INTEGER, Rating INTEGER);")
+
+    connection.commit()
+    db.close()
+    connection.close()
+
+def add_employee_rating(emp_ID, rating):
+    if not exists(db_name):
+        create_database(db_name)
+    
+
+    db_name = "employees.db"
+    connection = connect(database=db_name)
+    db = connection.cursor()
+
+    db.execute(f'INSERT INTO Ratings (EmpId,Rating) VALUES ("{emp_ID}","{rating}");')
+
+    connection.commit()
+    db.close()
+    connection.close()
+
+if __name__ == "__main__":
+    #test_list = search_by_loop("jon")
+    # print(test_list)
+    #search_by_regex("Jon")
+    #create_database()
+    #print(search_by_db("Parto"))
+    add_employee_rating(900, 5)
---
+++ Workshop_11/Task1/staff_directory(1).py
@@ -0,0 +1,83 @@
+#--------------------------------------------------------------------#
+#
+# Staff Directory Front-End
+#
+# This is the front-end of the staff directory application. It
+# creates a Graphical User Interface which can be used with any
+# of the back-end functions.
+#
+# To complete this exercise you need to work with your teammates
+# to decide who will implement the front-end GUI and who will
+# implement the back-end look-up function. You need to agree on
+# the signature of this function so that both teams can work
+# independently, i.e., what parameter(s) does the function accept
+# and what format does it return the search results in?
+#
+# Below is some skeletal code to help you get started on the
+# front-end, but feel free to design whatever GUI you want to
+# do the job.  
+#
+
+# Import the Tkinter functions
+from tkinter import *
+
+# Import the back-end function developed by the other team
+from backend_function import search_by_db
+
+
+#--------------------------------------------------------------------#
+# Define a function here to serve as the "command" when the user
+# initiates a search.  It should:
+#
+# 1) get the employee name entered by the user;
+# 2) call the back-end function to get the search results; and
+# 3) display the results in the GUI.
+#
+
+def searchEntry(Label, entrytext = None):
+    if entrytext == None:
+        return
+    
+    ##Put return data here to replace the text in the label
+    # Label['text'] = entrytext
+    for emp_no, birth_date, first_name, last_name, gender, hire_date in search_by_db(entrytext):
+        Label['state'] = 'normal'
+        Label.insert('1.0',f"{emp_no}: {first_name}, {last_name}, {birth_date}\n")
+        Label['state'] = 'disabled'
+    
+        
+#----------------------------------------------------------------#
+# The GUI front end
+#
+
+# Create a window
+staff_window = Tk()
+
+# Give the window a title
+staff_window.title('Staff Directory')
+
+# Create a Text widget to display the results
+displayFrame = Frame(staff_window, width=250, height=250, background="lightgrey", highlightbackground="grey", highlightthickness=1)
+displayFrame.pack(padx=5, pady=5)
+displayFrame.pack_propagate(0)
+displayLabel = Text(displayFrame, font=("calibre 10"), background="lightgrey")
+displayLabel.pack(padx=5, pady=5, anchor=W)
+displayLabel['state'] = 'disabled'
+# Create a text Entry widget for entering the employee name
+displayEntry = Entry(staff_window, background="lightgrey")
+displayEntry.pack(pady=5)
+
+# Create a Button widget to start the search
+displayButton = Button(staff_window, text="Enter", background="lightgrey", command=lambda:searchEntry(displayLabel, displayEntry.get()))
+displayButton.pack(pady=5)
+
+# Optional: Create any other widgets you want to make
+# the GUI look nice, such as Labels for instructions or
+# to display an image
+
+# Start the event loop
+staff_window.mainloop()
+
+#
+#--------------------------------------------------------------------#
+
